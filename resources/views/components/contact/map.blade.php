341f1cf144dcae70ee5144f148f0937ebd6fc1ae
James Philip Gomera <jpgomera19@gmail.com>
2026-02-25 16:47:45 +0800

    Changed the embedded map for pinning the location of McAsia Foodtrade Corporation

--- resources/views/components/contact/map.blade.php
+++ resources/views/components/contact/map.blade.php
@@ -5,12 +5,12 @@
             <p class="text-lg text-gray-600">Visit our showroom or connect with us virtually</p>
         </div>
 
-        <div class="rounded-2xl overflow-hidden border border-black/10 shadow-[0_20px_50px_rgba(18,18,18,0.14)]" data-aos="fade-up" data-aos-delay="100">
+        <div class="rounded-2xl overflow-hidden border border-black/10 shadow-[0_20px_50px_rgba(18,18,18,0.14)]"
+             data-aos="fade-up" data-aos-delay="100">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d811.5564961277616!2d120.99817532252506!3d14.631229248459263!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sph!4v1762151076371!5m2!1sen!2sph"
-                width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade" class="w-full">
-            </iframe>
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d965.109376777117!2d120.99776730422975!3d14.631088857569532!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b664691d8169%3A0xa01ff34f48ad9591!2sMcAsia%20Foodtrade%20Corporation!5e0!3m2!1sen!2sph!4v1772009093492!5m2!1sen!2sph"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade" class="w-full"></iframe>
         </div>
     </div>
 </section>
